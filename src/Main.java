591c6208f577674a62ec1876383a42b97fb37b62
tltl111 <tlafeber@outlook.com>
2023-05-15 19:59:52 +0200

    Working with flaws

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+import java.util.Random;
+
+
+class RandomNumberGuesser{
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner(System.in);
+        String game = "running";
+
+        while(game.equals("running"))
+        {
+            Random rand = new Random();
+            int randNum = rand.nextInt(10);
+            int guess = -1;
+            int attempts = 0;
+
+            while(guess != randNum)
+            {
+                System.out.print("Guess a number between 1 and 1000: ");
+                guess = reader.nextInt();
+                attempts++;
+
+                if(guess == randNum)
+                {
+                    System.out.println("Congraulations! You Win!");
+                    System.out.println("It took you " + attempts + " attempts to win the game.");
+                    System.out.print("Would you like to play again? Yes or No: ");
+                    game = reader.next().toLowerCase();
+                }
+                else if(guess > randNum)
+                {
+                    System.out.println("You guessed to high, try again.");
+                }
+                else
+                {
+                    System.out.println("You guessed to low, try again.");
+                }
+            }
+        }
+        reader.close();
     }
 }
