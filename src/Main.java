2613c31ad75e5144d0d2c89b8c510b8fbd9367ad
tltl111 <tlafeber@outlook.com>
2023-05-22 10:17:13 +0200

    Some formatting changes.

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import java.util.InputMismatchException;
 import java.util.Random;
 
 
-class RandomNumberGuesser{
-    public static void main(String[] args)
-    {
+class RandomNumberGuesser {
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         String game = "yes";
 
-        while(game.equals("yes") || game.equals("y"))
-        {
+        while(game.equals("yes") || game.equals("y")) {
             int lowest_number = 1;
             int highest_number = 1000;
             Random rand = new Random();
@@ -20,66 +18,54 @@ class RandomNumberGuesser{
             int attempts = 0;
             int max_attempts = 10;
 
-            while(guess != randNum && attempts != max_attempts)
-            {
+            while(guess != randNum && attempts != max_attempts) {
                 int attempts_left = max_attempts - attempts;
                 System.out.println("You have " + attempts_left + " attempts left.");
                 System.out.print("Guess a number between " + lowest_number + " and " + highest_number + " : ");
 
-                try
-                {
+                try {
                     guess = input.nextInt();
                     input.nextLine();
                 }
-                catch (InputMismatchException e)
-                {
+                catch (InputMismatchException e) {
                     System.out.println("You can only input a number.");
                     input.nextLine();
                     continue;
                 }
 
-                if(guess < lowest_number || guess > highest_number)
-                {
+                if(guess < lowest_number || guess > highest_number) {
                     System.out.println("\nYou can only guess between " + lowest_number + " and " + highest_number + ".");
                     continue;
                 }
-                else
-                {
+                else {
                     attempts++;
 
-                    if(guess == randNum)
-                    {
+                    if(guess == randNum) {
                         System.out.println("\nCongratulations! You Win!");
                         System.out.println("It took you " + attempts + " attempts to win the game.");
                         System.out.print("\nWould you like to play again? Yes or No: ");
                         game = input.next().toLowerCase();
                     }
-                    else if(guess > randNum)
-                    {
+                    else if(guess > randNum) {
                         System.out.print("\nYou guessed to high.");
-                        if(attempts < max_attempts)
-                        {
+                        if(attempts < max_attempts) {
                             System.out.println(" Try again.");
                             highest_number = guess;
                         }
-                        else
-                        {
+                        else {
                             System.out.println("\nYou lost!");
                             System.out.println("The correct number was " + randNum);
                             System.out.print("\nWould you like to play again? Yes or No: ");
                             game = input.next().toLowerCase();
                         }
                     }
-                    else
-                    {
+                    else {
                         System.out.print("\nYou guessed to low. ");
-                        if(attempts < max_attempts)
-                        {
+                        if(attempts < max_attempts) {
                             System.out.println(" Try again.");
                             lowest_number = guess;
                         }
-                        else
-                        {
+                        else {
                             System.out.println("\nYou lost!");
                             System.out.println("The correct number was " + randNum);
                             System.out.print("\nWould you like to play again? Yes or No: ");
